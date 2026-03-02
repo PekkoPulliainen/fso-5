99475a58ca597c0ec83762c29ddc9e030b3579d7
pekko <pekko.pulliainen@outlook.com>
2026-03-02 20:05:01 +0200

    5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [expand, setExpansion] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const showWhenVisible = { display: expand ? "none" : "" };
+  const hideWhenVisible = { display: expand ? "" : "none" };
+
+  return (
+    <div style={blogStyle}>
+      <div style={showWhenVisible}>
+        {blog.title} {blog.author}{" "}
+        <button onClick={() => setExpansion(!expand)}>expand</button>
+      </div>
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author}{" "}
+        <button onClick={() => setExpansion(!expand)}>hide</button>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes} <button>like</button>
+        </div>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
